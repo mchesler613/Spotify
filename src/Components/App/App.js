0472283cb1554db7d3d7717bf768b844a17b137e
mchesler613 <merilyngm@gmail.com>
2019-09-05 23:53:14 -0400

    Change title of page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -69,7 +69,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <h1>Spot <span className="highlight">Your</span> Music</h1>
         <div className="App">
           <SearchBar onSearch={this.search}  />
           <div className="App-playlist">
